f7945d3248a4cf9f7bb0b4422b0c1d79a6d9ffd4
Nader <139178@maximus.com>
2017-10-27 17:03:01 -0400

    Code clean up

--- js/main.js
+++ js/main.js
@@ -5,11 +5,14 @@
 //Suggested BUilds
 //Arcanes, AMPs, Zaws
 //fixing tables
+//auto sizing
+//warframe sections
+//suggested builds
 
 //Categories
-//Rank, MR Req, Notes, Name, suggested builds
+//MR Req, Notes, Name, base/event, Category
 //Revise:
-//CC Mechanic, Damage Type, Favors, base/event, role in group, mission type
+//CC Mechanic, Damage Type, Favors, role in group, mission type
 
 
 // Regular ol' javascript
@@ -24,60 +27,6 @@ let config = {
     messagingSenderId: "134665778083"
 };
 firebase.initializeApp(config);
-
-function sortTable(tablename, n) {
-    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    printname = tablename + '';
-    table = document.getElementById(tablename);
-    switching = true;
-    dir = "asc";
-    while (switching) {
-        //start by saying: no switching is done:
-        switching = false;
-        // rows = table.getElementsByTagName("TR");
-        rows = table.getElementsByTagName("TR");
-        /*Loop through all table rows (except the
-        first, which contains table headers):*/
-        for (i = 1; i < (rows.length - 1); i++) {
-            //start by saying there should be no switching:
-            shouldSwitch = false;
-            /*Get the two elements you want to compare,
-            one from current row and one from the next:*/
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            /*check if the two rows should switch place,
-            based on the direction, asc or desc:*/
-            if (dir === "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    //if so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir === "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                    //if so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-        if (shouldSwitch) {
-            /*If a switch has been marked, make the switch
-            and mark that a switch has been done:*/
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            //Each time a switch is done, increase this count by 1:
-            switchcount++;
-        } else {
-            /*If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again.*/
-            if (switchcount === 0 && dir === "asc") {
-                dir = "desc";
-                switching = true;
-            }
-        }
-    }
-}
 
 //Angular
 let app = angular.module("tierlist", []);
@@ -99,22 +48,76 @@ app.controller("TierListController", function TierListController ($scope) {
         console.log($scope.Primaries);
     });
 
-    firebase.database().ref('/Archmelees/').once('value').then(function (snapshot) {
+    firebase.database().ref('/Secondaries/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
-            $scope.Sentweps = $.map(snapshot.val(), function (element) {
+            $scope.Secondaries = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
-        console.log($scope.Sentweps);
+        console.log($scope.Secondaries);
+    });
+
+    firebase.database().ref('/Melees/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Melees = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Melees);
+    });
+
+    firebase.database().ref('/Frames/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Frames = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Frames);
+    });
+
+    firebase.database().ref('/Schools/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Schools = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Schools);
+    });
+
+    firebase.database().ref('/Archwingss/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Archwingss = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Archwingss);
+    });
+
+    firebase.database().ref('/Archguns/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Archguns = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Archguns);
+    });
+
+    firebase.database().ref('/Archmelees/').once('value').then(function (snapshot) {
+        $scope.$evalAsync(function () {
+            $scope.Archmelees = $.map(snapshot.val(), function (element) {
+                return element;
+            });
+        });
+        console.log($scope.Archmelees);
     });
 
     firebase.database().ref('/Companions/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
-            $scope.Sentweps = $.map(snapshot.val(), function (element) {
+            $scope.Companions = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
-        console.log($scope.Sentweps);
+        console.log($scope.Companions);
     });
 
     firebase.database().ref('/Sentweps/').once('value').then(function (snapshot) {
