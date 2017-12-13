8b42f583e2cb242325a54a0eaf1ccaec45f6e1f9
Nader <139178@maximus.com>
2017-12-13 15:00:41 -0500

    revert some changes, add some fixes

---
+++ README.md
@@ -0,0 +1,27 @@
+# i_wanna_b_the_guy's Tier List
+
+This README accompanies the i_wanna_b_the_guy's Warframe Tier List.
+
+## TODO
+
+This will be a more fine grained TODO as part of or seperate from issues
+listed in http://github.com/weblue/weblue.github.io/issues. Anything
+listed here is things I'm actively working on or thinking about.
+* Say "swag" a lot more
+* Figure out ordering mechanism before JSON editor page is created
+* Import frame builds from Kalgash's cheatsheet to here
+    * rank said builds
+* Figure out what to do about niche weapons and special flags in general
+* Start outlining the JSON editing page for those lucky, lucky curators
+;)
+
+## API Endpoints and Documentation
+
+This section outlines the current API endpoints and their functions.
+Need more endpoints or added robustness to the API? Feel free to submit
+an issue with your request
+
+#### Endpoints
+
+* /js/thelist.json
+    * This is a comprehensive output of all data in the Tierlist
--- js/main.js
+++ js/main.js
@@ -9,6 +9,21 @@ let config = {
 };
 firebase.initializeApp(config);
 
+let database;
+
+$.ajax({
+    type: 'GET',
+    dataType: 'application/json',
+    async: false,
+    //Change these when debugging
+    // url: 'http://localhost:63343/TierlistTemplate/js/thelist.json',
+    url: '/thelist.json',
+    complete: function(data) {
+        database = JSON.parse(data.responseText);
+    }
+});
+
+
 //Angular
 let app = angular.module("tierlist", []);
 
@@ -22,101 +37,147 @@ app.controller("TierListController", function TierListController ($scope) {
 
     $scope.loading = true;
 
-    firebase.database().ref('/version/').once('value').then(function (snapshot) {
+/*    firebase.database().ref('/version/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.version = snapshot.val();
         });
-    });
+    });*/
 
-    firebase.database().ref('/alerts/').once('value').then(function (snapshot) {
+    $scope.version = database.version;
+
+/*    firebase.database().ref('/alerts/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.alerts = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
-    });
+    });*/
 
+    $scope.alerts = $.map(database.alerts, function(element) {
+        return element;
+    });
+/*
     firebase.database().ref('/Primaries/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Primaries = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Primaries = $.map(database.Primaries, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Secondaries/').once('value').then(function (snapshot) {
+/*    firebase.database().ref('/Secondaries/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Secondaries = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Secondaries = $.map(database.Secondaries, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Melees/').once('value').then(function (snapshot) {
+/*    firebase.database().ref('/Melees/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Melees = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Melees = $.map(database.Melees, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Frames/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Frames/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Frames = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Frames = $.map(database.Frames, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Schools/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Schools/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Schools = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Schools = $.map(database.Schools, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Archwings/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Archwings/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Archwings = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Archwings = $.map(database.Archwings, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Archguns/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Archguns/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Archguns = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Archguns = $.map(database.Archguns, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Archmelees/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Archmelees/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Archmelees = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Archmelees = $.map(database.Archmelees, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Companions/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Companions/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Companions = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Companions = $.map(database.Companions, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Sentweps/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Sentweps/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function () {
             $scope.Sentweps = $.map(snapshot.val(), function (element) {
                 return element;
             });
         });
+    });*/
+
+    $scope.Sentweps = $.map(database.Sentweps, function(element) {
+        return element;
     });
 
-    firebase.database().ref('/Tiers/').once('value').then(function (snapshot) {
+    /*firebase.database().ref('/Tiers/').once('value').then(function (snapshot) {
         $scope.$evalAsync(function() {
             $scope.Tiers = $.map(snapshot.val(), function(element) {
                 return element;
@@ -130,8 +191,21 @@ app.controller("TierListController", function TierListController ($scope) {
                 });
             }, 2000);
         });
+    });*/
+
+    $scope.Tiers = $.map(database.Tiers, function(element) {
+        return element;
     });
 
-    $scope.categories = ['Primaries', 'Secondaries', 'Melee', /*'Frames',*/ 'Schools', 'Archwings', 'Archguns', 'Archmelees', 'Companions']
+    $('.loadingScreen').addClass("fadeOut");
 
+    setTimeout(function () {
+        $scope.$apply(function(){
+            $scope.loading = false;
+        });
+    }, 2000);
+
+    $scope.categories = ['Primaries', 'Secondaries', 'Melee', /*'Frames',*/ 'Schools', 'Archwings', 'Archguns', 'Archmelees', 'Companions'];
+
+    // firebase.database().goOffline();
 });
