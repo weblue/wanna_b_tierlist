fc428148488b0e62c95b3f1d6e3602cfef451f45
floon <floon@ssh.dreambot.org:floon.git>
2018-09-20 17:20:48 -0400

    removed sync ajax request and moved req to angular first try at fixing iphone scroll bug

--- js/main.js
+++ js/main.js
@@ -2,94 +2,90 @@
 //TODO add more columns
 //TODO add on hover information
 //TODO Change background
-//TODO domain name
 
 let database;
-
-$.ajax({
-    type: 'GET',
-    dataType: 'application/json',
-    async: false,
-    url: './js/thelist.json',
-    complete: function(data) {
-        database = JSON.parse(data.responseText);
-    }
-});
 
 //Angular
 let app = angular.module("tierlist", []);
 
 app.config([
-    '$interpolateProvider', function($interpolateProvider) {
+    '$interpolateProvider', function ($interpolateProvider) {
         return $interpolateProvider.startSymbol('{(').endSymbol(')}');
     }
 ]);
 
-app.controller("TierListController", function TierListController ($scope) {
+app.controller("TierListController", function TierListController($scope, $http) {
 
     $scope.loading = true;
 
-    $scope.version = database.version;
+    $http({
+        method: 'GET',
+        url: './js/thelist.json'
+    }).then(function successCallback(response) {
+        database = response.data;
 
-    $scope.alerts = $.map(database.alerts, function(element) {
-        return element;
-    });
+        $scope.version = database.version;
 
-    $scope.Primaries = $.map(database.Primaries, function(element) {
-        return element;
-    });
-
-    $scope.Secondaries = $.map(database.Secondaries, function(element) {
-        return element;
-    });
-
-    $scope.Melees = $.map(database.Melees, function(element) {
-        return element;
-    });
-
-    $scope.Frames = $.map(database.Frames, function(element) {
-        return element;
-    });
-
-    $scope.Schools = $.map(database.Schools, function(element) {
-        return element;
-    });
-
-    $scope.Archwings = $.map(database.Archwings, function(element) {
-        return element;
-    });
-
-    $scope.Archguns = $.map(database.Archguns, function(element) {
-        return element;
-    });
-
-    $scope.Archmelees = $.map(database.Archmelees, function(element) {
-        return element;
-    });
-
-    $scope.Companions = $.map(database.Companions, function(element) {
-        return element;
-    });
-
-    $scope.Sentweps = $.map(database.Sentweps, function(element) {
-        return element;
-    });
-
-    $scope.Tiers = $.map(database.Tiers, function(element) {
-        return element;
-    });
-
-    $scope.faq = $.map(database.faq, function(element) {
-        return element;
-    });
-
-    $('.loadingScreen').addClass("fadeOut");
-
-    setTimeout(function () {
-        $scope.$apply(function(){
-            $scope.loading = false;
+        $scope.alerts = $.map(database.alerts, function (element) {
+            return element;
         });
-    }, 2000);
+
+        $scope.Primaries = $.map(database.Primaries, function (element) {
+            return element;
+        });
+
+        $scope.Secondaries = $.map(database.Secondaries, function (element) {
+            return element;
+        });
+
+        $scope.Melees = $.map(database.Melees, function (element) {
+            return element;
+        });
+
+        $scope.Frames = $.map(database.Frames, function (element) {
+            return element;
+        });
+
+        $scope.Schools = $.map(database.Schools, function (element) {
+            return element;
+        });
+
+        $scope.Archwings = $.map(database.Archwings, function (element) {
+            return element;
+        });
+
+        $scope.Archguns = $.map(database.Archguns, function (element) {
+            return element;
+        });
+
+        $scope.Archmelees = $.map(database.Archmelees, function (element) {
+            return element;
+        });
+
+        $scope.Companions = $.map(database.Companions, function (element) {
+            return element;
+        });
+
+        $scope.Sentweps = $.map(database.Sentweps, function (element) {
+            return element;
+        });
+
+        $scope.Tiers = $.map(database.Tiers, function (element) {
+            return element;
+        });
+
+        $scope.faq = $.map(database.faq, function (element) {
+            return element;
+        });
+
+        $('.loadingScreen').addClass("fadeOut");
+
+        setTimeout(function () {
+            $scope.$apply(function () {
+                $scope.loading = false;
+            });
+        }, 2000);
+    });
 
     $scope.categories = ['Primaries', 'Secondaries', 'Melee', /*'Frames',*/ 'Schools', 'Archwings', 'Archguns', 'Archmelees', 'Companions'];
 
