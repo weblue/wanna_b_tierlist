d291a50fbc487af3f0c0633ab9d746879ef03cd3
Matthew Wang <matt@matthewwang.me>
2019-09-06 01:44:27 -0700

    makes tables slightly cleaner

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -14,7 +14,7 @@ import {Subscription} from "rxjs/internal/Subscription";
   styleUrls: ['./table.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('collapsed', style({height: '0px', minHeight: '0'})),
       state('expanded', style({height: '*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
