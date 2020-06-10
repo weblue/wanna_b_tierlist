bcfae6f5ff51dd45b8fcca8e003d403a12fdcb5d
floon <nader.kirrish@gmail.com>
2020-06-10 16:47:21 -0400

    changes to faq and finishing testing methods page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {FaqComponent} from "./components/faq/faq.component";
 import {MainComponent} from "./components/main/main.component";
+import {OroPageComponent} from "./components/oro-page/oro-page.component";
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'faq', component: FaqComponent }
+  { path: 'faq', component: FaqComponent },
+  { path: 'oro', component: OroPageComponent }
   ];
 
 @NgModule({
