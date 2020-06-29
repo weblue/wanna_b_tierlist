bb451c0269d1ea3af24ee1131529f2f24a87e61a
floon <nader.kirrish@gmail.com>
2020-06-28 23:04:40 -0400

    remove row animation

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -29,7 +29,7 @@ import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
     trigger('detailExpand', [
       state('collapsed', style({height: '0px', minHeight: '0'})),
       state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      // transition('expanded <=> collapsed', animate('400ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ]
 })
